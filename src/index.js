d57bb4dd596162778657fd9b2810cc0f178ba99d
Unbelievable <slavlen1999@gmail.com>
2018-10-09 21:27:14 +0300

    perf: improve the code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount( preferences = [] ) {
   let count = 0;
-  for (let i = 0; i <= preferences.length; i++){
-      if ((preferences[preferences[preferences[i]-1]-1] == i+1) && (preferences[i] != i+1)){
+
+  for (let i = 0; i < preferences.length; i++){
+      let firstPassionate = preferences[i] - 1;
+      let secondPassionate = preferences[firstPassionate] - 1;
+      let thirdPassionate = preferences[secondPassionate];
+
+      if ( (thirdPassionate === i + 1) && (firstPassionate !== i) ) {
           count++;
-          console.log(preferences[preferences[preferences[i] - 1] - 1]);
       }
   }
+
   return count/3;
-};
+}
